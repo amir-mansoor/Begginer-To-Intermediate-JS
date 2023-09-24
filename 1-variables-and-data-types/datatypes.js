7b5911a420467e287665482710ebbfcc972b4350
amir-mansoor <amirmansoor200@gmail.com>
2023-09-24 18:52:16 +0500

    data types

---
+++ 1-variables-and-data-types/datatypes.js
@@ -0,0 +1,76 @@
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
+
+
+let x = 16 + "Volvo";
+
+
+// let x = "16" + "Volvo";
+
+let y;       // Now y is undefined
+y = 5;       // Now y is a Number
+y = "John";  // Now y is a String
+
+// Using double quotes:
+let carName1 = "Volvo XC60";
+
+// Using single quotes:
+let carName2 = 'Volvo XC60';
+
+// Single quote inside double quotes:
+let answer1 = "It's alright";
+
+// Single quotes inside double quotes:
+let answer2 = "He is called 'Johnny'";
+
+// Double quotes inside single quotes:
+let answer3 = 'He is called "Johnny"';
+
+// With decimals:
+let x1 = 34.00;
+
+// Without decimals:
+let x2 = 34;
+
+// Exponention notation
+let y = 123e5;    // 12300000
+let z = 123e-5;   // 0.00123
+
+// Big integer
+ let x = BigInt("123456789012345678901234567890"); 
+
+// typeof operator 
+
+ typeof ""             // Returns "string"
+typeof "John"         // Returns "string"
+typeof "John Doe"     // Returns "string"
+//
+
+ typeof 0              // Returns "number"
+typeof 314            // Returns "number"
+typeof 3.14           // Returns "number"
+typeof (3)            // Returns "number"
+typeof (3 + 4)        // Returns "number"
+
+
+// datatype undefined
+let myname; // this is undefined
+
